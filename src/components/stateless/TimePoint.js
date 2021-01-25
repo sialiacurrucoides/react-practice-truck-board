8165ba22a51a19af765183d9d0c140572ecc644a
Simon Julia <julia@meska.hu>
2021-01-25 19:05:19 +0100

    flag effect

--- src/components/stateless/TimePoint.js
+++ src/components/stateless/TimePoint.js
@@ -4,6 +4,7 @@ function TimePoint({date, position}) {
     return (
         <div className="timePoint" style={{left: position}}>
             <div className="timePointBox">
+                <div className="flagEffect"></div>
                 <span className="dateText">{date}</span></div>
             <div className="verticalLine"></div>
         </div>
